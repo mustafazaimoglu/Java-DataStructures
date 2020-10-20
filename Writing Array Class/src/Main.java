0f9bab49469f27b0a18af1528e8aaec9063eddaf
Mustafa Zaimoğlu <mkuz138@gmail.com>
2020-10-20 21:19:40 +0200

    20.10.20

---
+++ Writing Array Class/src/Main.java
@@ -0,0 +1,84 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        Main m = new Main();
+        MyOwnArray mya = new MyOwnArray(10);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String choice;
+        int result;
+        
+        mkz:
+        while(true)
+        {
+            m.message();
+            choice = br.readLine();
+            
+            switch(choice)
+            {
+                case "1":
+                    System.out.print("Type the number that you want to add : ");
+                    result = Integer.parseInt(br.readLine());
+                    mya.add(result);
+                    break;
+                case "2":
+                    System.out.print("Type the number that you want to delete : ");
+                    result = Integer.parseInt(br.readLine());
+                    mya.delete(result);
+                    break;
+                case "3":
+                    System.out.print("Type the number that you want to update : ");
+                    result = Integer.parseInt(br.readLine());
+                    System.out.print("Type the new number : ");
+                    int newNumber = Integer.valueOf(br.readLine());
+                    mya.update(result,newNumber);
+                    break;
+                case "4":
+                    System.out.print("Type the numbers position that you want to get : ");
+                    result = Integer.parseInt(br.readLine());
+                    System.out.println("The number is " + mya.get(result));
+                    break;
+                case "5":
+                    System.out.print("Type the number that you want to find : ");
+                    result = Integer.parseInt(br.readLine());
+                    int position = mya.find(result);
+                    if(position == -1)
+                    {
+                        System.err.println("The number couldn't be found'");
+                    }
+                    else
+                    {
+                        System.out.println("The number's position is " + position);
+                    }
+                    break;
+                case "6":
+                    mya.display();
+                    break;
+                case "q":
+                    break mkz;
+                default:
+                    System.err.println("You made a mistake while choosing an option!");
+                    break;
+            }
+        }
+    }
+    
+    public void message()
+    {
+        System.out.println("************ MENU ************");
+        System.out.println("1 : Add \n"
+                + "2 : Delete \n"
+                + "3 : Update \n"
+                + "4 : Get \n"
+                + "5 : Find \n"
+                + "6 : Display \n"
+                + "q : Quit");
+        System.out.print("Your Choice : ");
+    }
+}
---
+++ Writing Array Class/src/MyOwnArray.java
@@ -0,0 +1,110 @@
+
+import java.util.Arrays;
+
+public class MyOwnArray
+{
+
+    private int arrLength;
+    private int arr[];
+    private int instantIndex;
+
+    public MyOwnArray(int arrLength)
+    {
+        this.arrLength = arrLength;
+        arr = new int[this.arrLength];
+        instantIndex = 0;
+    }
+
+    public void add(int x)
+    {
+        if (instantIndex == arrLength)
+        {
+            System.err.println("You have reached the bound!");
+        }
+        else
+        {
+            arr[this.instantIndex] = x;
+            instantIndex++;
+        }
+    }
+    
+    public int get(int position) 
+    {
+        if(position > this.instantIndex)
+        {
+            throw new ArrayIndexOutOfBoundsException("You crossed the bound!");
+        }
+        else
+        {
+            return arr[position];
+        }
+
+    }
+
+    public int find(int x)
+    {
+        int result = -1;
+        for (int i = 0; i < instantIndex; i++)
+        {
+            if (x == arr[i])
+            {
+                result = i;
+            }
+        }
+
+        return result;
+    }
+
+    public void delete(int x)
+    {
+        int toDelete = this.find(x);
+
+        if (toDelete != -1)
+        {
+            while (toDelete < instantIndex - 1)
+            {
+                arr[toDelete] = arr[toDelete + 1];
+                toDelete++;
+            }
+
+            instantIndex--;
+        }
+        else
+        {
+            System.err.println("Element not found!");
+        }
+
+    }
+
+    public void update(int x, int newValue)
+    {
+        int toUpdate = this.find(x);
+
+        if (toUpdate != -1)
+        {
+            arr[toUpdate] = newValue;
+            System.out.println("Mission Completed!");
+        }
+        else
+        {
+            System.err.println("Element not found!");
+        }
+    }
+    
+    public void display()
+    {
+        System.out.print("[");
+        for (int i = 0; i < this.instantIndex; i++)
+        {
+            if (i != 0)
+            {
+                System.out.print(", ");
+            }
+            System.out.print(arr[i]);
+        }
+        System.out.print("]\n");
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
